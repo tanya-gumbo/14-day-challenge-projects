527f33656243e65c0b7a2eb82e97eb1aaddbd614
tanya-gumbo <tanyagumbo05@gmail.com>
2025-07-19 23:10:16 +0200

    feat(js): Add functionality to the socials button

---
+++ business_card/logic.js
@@ -0,0 +1,14 @@
+github = document.getElementById('github')
+github.addEventListener('click', function() {
+    window.open('https://github.com', '_blank');
+  });
+
+gmail = document.getElementById('email')
+gmail.addEventListener('click', function() {
+    window.open('mailto:someone@example.com', '_blank');
+  });
+
+linkedIn = document.getElementById('linkedIn')
+linkedIn.addEventListener('click', function() {
+    window.open('https://linkedin.com', '_blank');
+  });
